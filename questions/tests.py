92d5f73f85d9b31613629d4d1538634b264ef0f5
Lance Goyke <lancegoyke@gmail.com>
2023-01-21 16:43:36 -0700

    add tests

--- questions/tests.py
+++ questions/tests.py
@@ -1,14 +1,19 @@
-from django.test import TestCase, Client
+from django.db import IntegrityError
+from django.forms import ModelForm
+from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 
-from .models import Question
+from .filters import QuestionFilter
+from .models import Question, Submission
 
-# Create your tests here.
+
 class QuestionTests(TestCase):
 
     user_username = 'user'
+    user_firstname = "John"
+    user_lastname = "Doe"
     user_email = 'user@email.com'
     user_password = 'userpassword'
     superuser_username = 'superuser'
@@ -18,6 +23,8 @@ class QuestionTests(TestCase):
     def setUp(self):
         # Create users
         self.user = get_user_model().objects.create_user(
+            first_name=self.user_firstname,
+            last_name=self.user_lastname,
             username=self.user_username,
             email=self.user_email,
             password=self.user_password
@@ -62,22 +69,10 @@ class QuestionTests(TestCase):
     def test_question_list_view(self):
         response = self.client.get(reverse('question_filtered_list'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Filter Questions')
+        self.assertContains(response, 'Filter questions')
         self.assertContains(response, 'New public question')
         self.assertContains(response, 'New private question')
         self.assertTemplateUsed(response, 'questions/question_filtered_list.html')
-
-    def test_question_list_view_questiontextcontains_filter(self):
-        response = self.client.get(reverse('question_filtered_list') + '?question_text__contains=public')
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'New public question')
-        self.assertNotContains(response, 'New private question')
-
-    def test_question_list_view_askerfirstnamecontains_filter(self):
-        pass
-
-    def test_question_list_view_askerlastnamecontains_filter(self):
-        pass
 
     def test_public_question_detail_view(self):
         response = self.client.get(self.public_question.get_absolute_url())
@@ -99,14 +94,87 @@ class QuestionTests(TestCase):
         self.assertContains(response, 'not yet been answered')
         self.assertTemplateUsed(response, 'questions/question_detail.html')
 
+    def test_question_list_view_questiontextcontains_filter(self):
+        qs = Question.objects.all()
+        query = {"question_text__icontains": "public"}
+        f = QuestionFilter(query, queryset=qs)
+        self.assertQuerysetEqual(
+            f.qs,
+            [self.public_question.pk],
+            lambda o: o.pk,
+            ordered=False
+        )
+
+    def test_question_list_view_askerfirstnamecontains_filter(self):
+        qs = Question.objects.all()
+        query = {"asker__first_name__icontains": self.user_firstname}
+        f = QuestionFilter(query, queryset=qs)
+        self.assertQuerysetEqual(
+            f.qs,
+            [self.public_question.pk, self.anonymous_question.pk],
+            lambda o: o.pk,
+            ordered=False
+        )
+
+    def test_question_list_view_askerlastnamecontains_filter(self):
+        qs = Question.objects.all()
+        query = {"asker__last_name__icontains": self.user_lastname}
+        f = QuestionFilter(query, queryset=qs)
+        self.assertQuerysetEqual(
+            f.qs,
+            [self.public_question.pk, self.anonymous_question.pk],
+            lambda o: o.pk,
+            ordered=False
+        )
+
+    def test_question_update_view_does_not_allow_generic_user(self):
+        self.client.login(username=self.user_username, password=self.user_password)
+        response = self.client.get(self.public_question.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+
+    def test_question_update_view_allows_superuser(self):
+        self.client.login(username=self.superuser_username, password=self.superuser_password)
+        response = self.client.get(self.public_question.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+
 
 class SubmissionTests(TestCase):
 
+    user_username = 'user'
+    user_email = 'user@email.com'
+    user_password = 'userpassword'
+    question_1 = 'What is the best way to teach someone how to deadlift?'
+    question_2 = 'How can I convince a busy professional to work on their recovery?'
+
     def setUp(self):
-        pass
+        self.user = get_user_model().objects.create_user(
+            username=self.user_username,
+            email=self.user_email,
+            password=self.user_password
+        )
 
-    def test_submission_template(self):
-        pass
+    def test_submission_with_no_question(self):
+        with self.assertRaises(IntegrityError):
+            Submission.objects.create(submitted_by=self.user)
 
-    def test_submission_form(self):
-        pass
+    def test_submission_with_one_question(self):
+        submission = Submission.objects.create(
+            question_1=self.question_1,
+            submitted_by=self.user
+        )
+        self.assertEqual(submission.question_1, self.question_1)
+
+    def test_submission_with_two_questions(self):
+        submission = Submission.objects.create(
+            question_1=self.question_1,
+            question_2=self.question_2,
+            submitted_by=self.user
+        )
+        self.assertEqual(submission.question_1, self.question_1)
+        self.assertEqual(submission.question_2, self.question_2)
+
+    def test_additional_notes(self):
+        submission = Submission.objects.create(
+            question_1=self.question_1,
+            submitted_by=self.user
+        )
